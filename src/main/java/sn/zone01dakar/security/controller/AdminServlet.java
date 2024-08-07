f3d14ad75b087a558cba55b5b2b41934b131a1e8
bayerane <bayerane89@hotmail.fr>
2024-08-07 12:04:56 +0000

    feat: jstl

--- src/main/java/sn/zone01dakar/security/controller/AdminServlet.java
+++ src/main/java/sn/zone01dakar/security/controller/AdminServlet.java
@@ -8,11 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+import sn.zone01dakar.security.dto.UserDto;
+
 @WebServlet(name = "admin", value = "/admin")
 public class AdminServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		List<UserDto> users = new ArrayList<UserDto>();
+		UserDto user1 = new UserDto(1, "Alassane", "DIOP", "dalassane@gmail.com");
+		UserDto user2 = new UserDto(2, "Baye Rane", "DIOP", "dbayerane@gmail.com");
+		UserDto user3 = new UserDto(3, "Michel", "KRuff", "mkruff@gmail.com");
+		
+		users.add(user1);
+		users.add(user2);
+		users.add(user3);
+		
+		req.setAttribute("usersList", users);
 		req.getRequestDispatcher("WEB-INF/jsp/users/list.jsp").forward(req, resp);
 	}
 	
--- src/main/webapp/WEB-INF/jsp/users/list.jsp
+++ src/main/webapp/WEB-INF/jsp/users/list.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,6 +8,28 @@
 		<title>Insert title here</title>
 	</head>
 	<body>
-		Liste
+		<jsp:include page="../welcome.jsp"></jsp:include>
+		<div class="container">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>First name</th>
+						<th>Last name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<c:forEach items="${ usersList }" var="user">
+						<tr>
+							<td>${ user.id }</td>
+							<td>${ user.firstName }</td>
+							<td>${ user.lastName }</td>
+							<td>${ user.email }</td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</table>
+		</div>	
 	</body>
 </html>
