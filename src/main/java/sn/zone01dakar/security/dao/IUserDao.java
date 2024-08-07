2e976f1f22541eec122269e9b78e249cce6c184f
bayerane <bayerane89@hotmail.fr>
2024-08-07 14:22:32 +0000

    feat: docker et hibernate

---
+++ src/main/java/sn/zone01dakar/security/dao/IUserDao.java
@@ -0,0 +1,7 @@
+package sn.zone01dakar.security.dao;
+
+import sn.zone01dakar.security.entity.UserEntity;
+
+public interface IUserDao extends Repository<UserEntity> {
+
+}
---
+++ src/main/java/sn/zone01dakar/security/dao/UserDao.java
@@ -0,0 +1,7 @@
+package sn.zone01dakar.security.dao;
+
+import sn.zone01dakar.security.entity.UserEntity;
+
+public class UserDao extends RepositoryImpl<UserEntity> implements IUserDao {
+
+}
